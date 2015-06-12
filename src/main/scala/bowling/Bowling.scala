b89e25c5d51c0e6c5f59ac96bc41ab1026c91bbd
Maria del Mar Rubio-Gonzalez <mariadelmarrg@gmail.com>
2015-06-12 12:50:38 +0200

    Removed isStrike and isSpare from case class fields

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -1,7 +1,9 @@
 package bowling
 
-case class Frame(firstBall: Int, secondBall: Option[Int] = None, isSpare: Boolean = false, isStrike: Boolean = false, isBonus: Boolean = false) {
+case class Frame(firstBall: Int, secondBall: Option[Int] = None, isBonus: Boolean = false) {
   def score = firstBall + secondBall.getOrElse(0)
+  def isStrike = firstBall == 10 && secondBall.isEmpty
+  def isSpare = secondBall.isDefined && score == 10
 }
 
 object Bowling {
@@ -14,34 +16,34 @@ object Bowling {
     calculate(frames.toList)
   }
 
-  private val missSecondBall = "([1-9]\\-)".r
-  private val missFirstBall = "(\\-[1-9])".r
-  private val twoBalls = "([1-9])([1-9])".r
-  private val spare = "([1-9])(\\/)".r
-  private val strike = "X"
-  private val twoStrikesInBonus = "XX"
-  private val missTwoBalls = "--"
+  private val MISS_SECOND_BALL = "([1-9]\\-)".r
+  private val MISS_FIRST_BALL = "(\\-[1-9])".r
+  private val TWO_BALLS = "([1-9])([1-9])".r
+  private val SPARE = "([1-9])(\\/)".r
+  private val STRIKE = "X"
+  private val TWO_STRIKES_IN_BONUS = "XX"
+  private val MISS_TWO_BALLS = "--"
 
   private def buildFrame(frame: String): Frame = {
 
     def toInt(value: String, pos: Int) = Integer.parseInt(value.charAt(pos).toString)
 
     frame match {
-      case `strike` =>
-        Frame(10, None, isStrike = true)
-      case `twoStrikesInBonus` =>
+      case STRIKE =>
+        Frame(10, None)
+      case TWO_STRIKES_IN_BONUS =>
         Frame(10, Some(10))
-      case `missTwoBalls` =>
+      case MISS_TWO_BALLS =>
         Frame(0, Some(0))
-      case spare(firstBall, secondBall) =>
+      case SPARE(firstBall, secondBall) =>
         val firstBallPins = toInt(firstBall, 0)
         val secondBallPins = 10 - firstBallPins
-        Frame(firstBallPins, Some(secondBallPins), isSpare = true)
-      case missSecondBall(value) =>
+        Frame(firstBallPins, Some(secondBallPins))
+      case MISS_SECOND_BALL(value) =>
         Frame(toInt(value, 0), Some(0))
-      case missFirstBall(value) =>
+      case MISS_FIRST_BALL(value) =>
         Frame(0, Some(toInt(value, 1)))
-      case twoBalls(firstBall, secondBall) =>
+      case TWO_BALLS(firstBall, secondBall) =>
         Frame(firstBall.toInt, Some(secondBall.toInt))
     }
   }
